96d681cd3572052dadf4a1ee1ae812b822b42246
Bhupinder Singh <singhbhupinderkahlon@gmail.com>
2020-06-18 21:15:29 -0700

    comments added.

--- bash/diceroller.sh
+++ bash/diceroller.sh
@@ -6,6 +6,7 @@
 # Task: Improve this script by making it roll as many dice as there are letters in your first name, instead of just one
 
 # roll the dice and display the result
+# my name is of 9 words so dice will be rolled 9 times
 echo "
 Rolling...
 $(( RANDOM % 6 + 9)) rolled
